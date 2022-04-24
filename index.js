89f3efe3cd954d5796893bfa5d3fe3843d5bcd41
Khaled Karim <kskarim177@gmail.com>
2022-04-24 20:08:12 +0600

    server is ready

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mzzkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            console.log(services)
+            res.send(services);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // POST
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // DELETE 
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('running my genius server');
+});
+
+app.listen(port, () => {
+    console.log(port)
+})
